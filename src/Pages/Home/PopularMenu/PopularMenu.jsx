b5be7673367007695a43f0d14acad1b172e8dcf5
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-14 02:52:19 +0800

    added popular menu title and add this component in the home page

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+
+
+const PopularMenu = () => {
+    return (
+        <section className="mb-12">
+        <SectionTitle
+            heading="From Our Menu"
+            subHeading="Popular Items"
+        ></SectionTitle>
+        </section>
+    );
+};
+
+export default PopularMenu;

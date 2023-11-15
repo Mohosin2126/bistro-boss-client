33f05975a55d551902e9678e56d4a6221752de5a
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-15 19:16:59 +0800

    data loaded and filterd

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 
 const PopularMenu = () => {
+const [menu,setMenu]=useState([])
+
+    useEffect(()=>{
+        fetch("menu.json")
+        .then(res=>res.json())
+        .then(data=>{
+            const popularItems=data.filter(item=>item.category==="popular")
+            setMenu(popularItems)
+
+        })
+    },[])
+console.log(menu)
+
     return (
         <section className="mb-12">
         <SectionTitle
             heading="From Our Menu"
             subHeading="Popular Items"
         ></SectionTitle>
+        <div>
+        </div>
         </section>
     );
 };

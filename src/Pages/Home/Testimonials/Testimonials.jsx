c9545e9e383531e16d68ffe557d316ab8fb9eb5f
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-15 21:20:55 +0800

    react rating added in testimonial section

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -8,6 +8,8 @@ import 'swiper/css/navigation';
 // import required modules
 import { Navigation } from 'swiper/modules';
 import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
 
 const Testimonials = () => {
 
@@ -32,6 +34,13 @@ const Testimonials = () => {
                     reviews.map(review => <SwiperSlide key={review._id}>
 
                         <div className="flex flex-col items-center mx-24 my-16">
+
+                        <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+
                             <p className="py-8">{review.details}</p>
                             <h3 className="text-2xl text-orange-400">{review.name}</h3>
                         </div>

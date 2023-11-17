8d822a526dfa759ec2709d8c48fbb63d7aa2633b
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-17 20:25:18 +0800

    added popular component in menu component

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -2,6 +2,7 @@ import { Helmet} from 'react-helmet-async';
 import Cover from '../Shared/Cover/Cover';
 
 import menuImg from"../../assets/menu/menu-bg.jpg"
+import PopularMenu from '../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -9,7 +10,18 @@ const Menu = () => {
             <Helmet>
         <title>Bistro|Menu</title>
       </Helmet>
-       <Cover img={menuImg} title='Our menu'></Cover>
+      <div>
+      <Cover img={menuImg} title='Our menu'></Cover>
+       <PopularMenu></PopularMenu>
+      </div>
+      <div>
+      <Cover img={menuImg} title='Our menu'></Cover>
+       <PopularMenu></PopularMenu>
+      </div>
+      <div>
+      <Cover img={menuImg} title='Our menu'></Cover>
+       <PopularMenu></PopularMenu>
+      </div>
         </div>
     );
 };

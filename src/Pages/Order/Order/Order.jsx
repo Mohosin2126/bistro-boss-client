ecca40cb37e2a7e88440fe44f0a0aa56a7a5fc19
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-17 21:58:09 +0800

    added food card and elments

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -3,8 +3,16 @@ import orderCoverImg from"../../../assets/shop/order.jpg"
 import Cover from "../../Shared/Cover/Cover";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import useMenu from "../../../hooks/UseMenu";
 const Order = () => {
     const [tabIndex,setTabIndex]=useState(0)
+    const [menu]=useMenu()
+    const deserts=menu.filter(item=>item.category==='dessert')
+    const soup=menu.filter(item=>item.category==='soup')
+    const salad=menu.filter(item=>item.category==='salad')
+    const pizza=menu.filter(item=>item.category==='pizza')
+    const offered=menu.filter(item=>item.category==='offered')
+       
     return (
         <div>
            <Cover img={orderCoverImg} title="Order Food"></Cover>

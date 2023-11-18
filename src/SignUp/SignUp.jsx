cd587669b3582669508ec3ab52f3329dfa022aed
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-18 21:22:16 +0800

    input field validation done

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -34,29 +34,29 @@ const onSubmit=data=>{
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" {...register("name")}   name="name" placeholder="Name" className="input input-bordered" />
-                                 <span className="text-red-600">Name is required</span>
+                                <input type="text" {...register("name",{ required: true })}   name="name" placeholder="Name" className="input input-bordered" />
+                                {errors.name && <span className="text-red-600">Name is required</span>} 
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Photo URL</span>
                                 </label>
-                                <input type="text" {...register("photourl")} placeholder="Photo URL" className="input input-bordered" />
-                                <span className="text-red-600">Photo URL is required</span>
+                                <input type="text" {...register("photourl",{ required: true })} placeholder="Photo URL" className="input input-bordered" />
+                                {errors.photourl && <span className="text-red-600">Photo URL is required</span>} 
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" {...register("email")} name="email" placeholder="email" className="input input-bordered" />
-                                 <span className="text-red-600">Email is required</span>
+                                <input type="email" {...register("email",{ required: true })} name="email" placeholder="email" className="input input-bordered" />
+                                {errors.email && <span className="text-red-600">Email is required</span>} 
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" {...register("password")} placeholder="password" className="input input-bordered" />
-            <span className="text-red-600">Password is required</span>
+                                <input type="password" {...register("password",{ required: true })} placeholder="password" className="input input-bordered" />
+                                {errors.password && <span className="text-red-600">Password is required</span>} 
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>

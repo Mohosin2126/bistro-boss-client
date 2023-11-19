3623eae00ad7cf69bf35f5b10932b4600227a499
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-20 04:19:13 +0800

    calculeted total price using reduce

--- src/Pages/DashBoard/Cart/Cart.jsx
+++ src/Pages/DashBoard/Cart/Cart.jsx
@@ -3,11 +3,12 @@ import useCart from "../../../hooks/useCart";
 const Cart = () => {
     const [cart] = useCart();
     console.log(cart)
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
     return (
         <div>
         <div className="flex justify-evenly mb-8">
             <h2 className="text-4xl">Items: {cart.length}</h2>
-            <h2 className="text-4xl">Total Price</h2>
+            <h2 className="text-4xl">Total Price: ${totalPrice}</h2>
             <button className="btn btn-primary">Pay</button>
 
         </div>

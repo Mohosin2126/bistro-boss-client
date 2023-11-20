570b40f96161ed6df58e98bf11d685b51bbe5f70
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-21 04:25:50 +0800

    add additem and set it's route in main

--- src/main.jsx
+++ src/main.jsx
@@ -23,6 +23,7 @@ import {
 import DashBoard from './Layout/DashBoard';
 import Cart from './Pages/DashBoard/Cart/Cart';
 import AllUsers from './Pages/DashBoard/AllUsers/AllUsers';
+import AddItems from './Pages/DashBoard/AddItems/AddItems';
 
 const queryClient = new QueryClient();
 
@@ -70,7 +71,11 @@ const router = createBrowserRouter([
 // admin routes 
 {
   path:"/dashboard/users",
-  element:<AllUsers></AllUsers>
+  element:<AllUsers></AllUsers>,
+},
+{
+  path:"/dashboard/addItems",
+  element:<AddItems></AddItems>
 }
    ],
   },

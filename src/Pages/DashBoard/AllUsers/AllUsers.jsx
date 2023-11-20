28b83c184abb694302d6b5a3ba94310dd64ea807
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-20 20:11:16 +0800

    deleted function done

--- src/Pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/Pages/DashBoard/AllUsers/AllUsers.jsx
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import UseAxiosSecure from "../../../hooks/UseAxiosSecure";
 import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 const AllUsers = () => {
 const axiosSecure=UseAxiosSecure()
 
-const {data:users=[]}=useQuery({
+const {data:users=[],refetch}=useQuery({
     queryKey:["users"],
     queryFn:async()=>{
         const res =await axiosSecure.get("/users")
@@ -12,6 +13,33 @@ const {data:users=[]}=useQuery({
 
     }
 })
+   
+const handleDeleteUser = user => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            axiosSecure.delete(`/users/${user._id}`)
+                .then(res => {
+                    if (res.data.deletedCount > 0) {
+                        refetch();
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                        });
+                    }
+                })
+        }
+    });
+}
 
 
     return (
@@ -47,6 +75,7 @@ const {data:users=[]}=useQuery({
                             </td>
                             <td>
                                 <button
+                                onClick={()=>handleDeleteUser(user)}
                                     className="btn btn-ghost btn-lg">
                                     <FaTrashAlt className="text-red-600"></FaTrashAlt>
                                 </button>

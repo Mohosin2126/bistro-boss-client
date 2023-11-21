6cf266db5a1805330660f3ad5b713a0e8fe7e132
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-21 17:27:43 +0800

    img uploaded in imgbb

--- src/Pages/DashBoard/AddItems/AddItems.jsx
+++ src/Pages/DashBoard/AddItems/AddItems.jsx
@@ -1,20 +1,28 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 
 
-// const image_hosting_key=import.meta.env.VITE_IMAGE_HOSTING_KEY
-// const image_hosting_api=`https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+const image_hosting_key=import.meta.env.VITE_IMAGE_HOSTING_KEY
+const image_hosting_api=`https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+
+
 
 const AddItems = () => {
+    const axiosPublic=useAxiosPublic()
+
     const { register, handleSubmit, reset } = useForm();
-const onSubmit=(data)=>{
+const onSubmit=async(data)=>{
     
 // image upload to imgbb and then get an url 
-console.log(data)
-
-
-
+const imageFile={image:data.image[0]}
+const res = await axiosPublic.post(image_hosting_api, imageFile, {
+    headers: {
+        'content-type': 'multipart/form-data'
+    }
+});
+console.log(res.data)
     reset()
 }
 
